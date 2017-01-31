1711ac7256a73e5dcce15ef1ee8612d816f52288
schalkneethling <sneethling@mozilla.com>
2017-01-31 12:08:26 +0200

    add environmental vars

--- src/config.js
+++ src/config.js
@@ -5,18 +5,9 @@
  * access_token_secret
  */
 
-// Uncomment for when deploying to Heroku
-
-// module.exports = {
-//   consumer_key: process.env.CONSUMER_KEY,
-//   consumer_secret: process.env.CONSUMER_SECRET,
-//   access_token: process.env.ACCESS_TOKEN,
-//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-// };
-
 module.exports = {
-  consumer_key: 'iJnQkYMlrONEtZtfjJlZsnWCi',
-  consumer_secret: 'fUxHmGEKI2UrqBd6HkIqNhcweroeq1ZOviEKUZQefNGZQaaqlr',
-  access_token: '822029015577952256-2QmjokJqTgbXWdDCFOggz2DirARgl2f',
-  access_token_secret: 'YQhSBFQSVR57G8A8UfrCfbqtkvMLLoT7Lm2zAzvKtYZkO',
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 };
